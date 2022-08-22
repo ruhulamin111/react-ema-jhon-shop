7ef9c81cf6a6f817972ed7ed380ee9b204e6343e
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-22 09:26:56 +0600

    fetch data done

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Shop.css'
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/ProgrammingHero1/ema-john-resources/main/fakeData/products.json')
+            .then(res => res.json())
+            .then(data => setProducts(data))
 
+    }, []);
 
     return (
         <div className='container'>
