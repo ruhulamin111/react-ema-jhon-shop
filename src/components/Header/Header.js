087eed11d162558ad87d1ed233afe4b16609f348
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-22 08:45:17 +0600

    header option add

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../images/Logo.svg';
+
+const Header = () => {
+    return (
+        <div className='header-top'>
+            <div className='header'>
+                <div>
+                    <a href="#home">
+                        <img src={logo} alt="" />
+                    </a>
+                </div>
+                <div>
+                    <a href="#home">Home</a>
+                    <a href="#shop">Shop</a>
+                    <a href="#order">Order</a>
+                    <a href="#about">About</a>
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Header;
