0cc050a38934c33e374a74b8ef91d9be1af9aa35
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-27 23:10:39 +0600

    add cart item design done

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,8 +1,10 @@
+import { faDeleteLeft, faRemove } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './CartItem.css';
 
 const CartItem = ({ product }) => {
-    const { price, name, img } = product;
+    const { price, name, img, seller } = product;
 
     return (
         <div className='item'>
@@ -11,11 +13,12 @@ const CartItem = ({ product }) => {
             </div>
             <div className='text-container'>
                 <div>
-                    <h3>{name.slice(0, 15)}</h3>
+                    <h3>{name.slice(0, 20)}</h3>
                     <p>Price: <b>{price} $</b></p>
+                    <p>Company: <b>{seller}</b></p>
                 </div>
-                <div className='butto'>
-                    <button>Order Now</button>
+                <div className='remove'>
+                    <button> <i><FontAwesomeIcon icon={faRemove}></FontAwesomeIcon></i></button>
                 </div>
             </div>
         </div>
