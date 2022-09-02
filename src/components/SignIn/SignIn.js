f97345ba4f6e84507523cbbfb4ad95aa46ecacac
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-02 08:56:08 +0600

    style

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -8,11 +8,14 @@ const SignIn = () => {
         <div className='login-container'>
             <h2>Sign In</h2>
             <form>
+                <label htmlFor="">Email</label>
+                <input type="email" name="" placeholder='email' />
+                <label htmlFor="">Password</label>
+                <input type="password" name="" placeholder='password' />
+                <input className='submit' type="submit" value="Sign In" />
 
             </form>
-            <div>
 
-            </div>
         </div>
     );
 };
